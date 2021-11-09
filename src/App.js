30788264c8e2a749322092798e0e038046373843
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-09 16:56:22 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import'./App.css';
+import { Container } from '@material-ui/core';
+import Header from './components/header/Header';
+
+
 function App() {
+
+  const [word, setWord] = useState('');
+  const [meanings, setMeanings] = useState([]);
+  const [category, setCategory] = useState('en');
+
+const dictionaryApi = async () => {
+
+  try {
+
+    const data  = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/victor")
+    console.log(data.data)
+
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+useEffect(() => {
+  dictionaryApi()
+}, []);
+
+
+
+
   return (
-      <div>
-        This is the app!
+      <div style={{height:'100vh', backgroundColor:'#282c34', color:'white'}}>  
+        <Container maxWidth='md' style={{display:'flex', flexDirection:'column', height:'100vh'}}   >
+          <Header category={category} setCategory={setCategory} word={word} setWord={setWord} />
+        </Container>
       </div>
   );
 }
