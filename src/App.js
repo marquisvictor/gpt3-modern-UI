964f594dd7ec36e0038c77ca58206e2f5ed05761
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-14 23:17:46 +0100

    contd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import'./App.css';
-import { Container, FormControlLabel, FormGroup, styled, Switch } from '@material-ui/core';
-import Header from './components/header/Header';
-import Definitions from './Definitions/Definitions';
-
+import './App.css';
 
 function App() {
-
- 
-  // '#282c34'
-
-
-  return (
-      <div style={{height:'100vh', backgroundColor: `${mode ? '#282c34': '#245c36'}`, color:'white', transition: 'all 0.4s linear'}}>  
-        Hello world
-      </div>
-  );
+    return <div>Hello world</div>;
 }
 
 export default App;
