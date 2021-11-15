012b64c2de7772d312cc2aa54569690a9310ce45
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-15 01:11:37 +0100

    add all the logos and study BEM notation style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import {
+    Footer,
+    Blog,
+    Possibility,
+    Features,
+    Header,
+    WhatGPT3,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-    return <div>Hello world</div>;
+    return (
+        <div className='app'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <footer />
+        </div>
+    );
 }
 
 export default App;
