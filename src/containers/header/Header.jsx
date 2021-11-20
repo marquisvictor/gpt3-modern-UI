de7a63a24b296b95548dac2badcb55dacf598dcb
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-21 00:24:12 +0100

    add media queries for 1250 screens

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 import './header.css';
 
 const Header = () => {
@@ -14,9 +16,26 @@ const Header = () => {
                     everything joy alteration boisterous the attachment. Party
                     we years to order allow asked of.
                 </p>
+                <div className='header__input-button'>
+                    <input type='text' placeholder='Your Email Address' />
+                    <button type='button'>
+                        <p className='header__font'>Get Started</p>
+                    </button>
+                </div>
+                <div className='header__people'>
+                    <img src={people} alt='people' />
+                    <p>
+                        1,600 people requested access a visit in last 24 hours
+                    </p>
+                </div>
             </div>
-
-            <div className='header-right'>image</div>
+            <div className='header__img-container'>
+                <img
+                    className='header__img'
+                    src={ai}
+                    alt='Artificial intelligence'
+                />
+            </div>
         </div>
     );
 };
