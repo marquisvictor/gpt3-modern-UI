0ce7656afa7bc1567d1628c244730128df368010
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-22 15:03:16 +0100

    finish the whatgpt3 page, remains the media-queries

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = ({ header, text }) => {
+const Feature = ({ header, text, styledisplay, styleline }) => {
     return (
         <>
-            <div className='line'></div>
-            <div className='feature__container'>
+            <div className={`feature__container ${styledisplay}`}>
+                <span className={`line ${styleline}`}></span>
                 <h3 className='feature__header'>{header}</h3>
                 <p className='feature__text'>{text}</p>
             </div>
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -7,18 +7,48 @@ const WhatGPT3 = () => {
     const body1 =
         'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.';
 
+    const header2 = 'Chatbots';
+    const body2 =
+        'We so opinion friends me message as delight. Whole front do of plate heard oh ought. ';
+
+    const header3 = 'Knowledgebase';
+    const body3 =
+        'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b';
+
+    const header4 = 'Education';
+    const body4 =
+        'At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b';
+
+    const Line = () => <span className='line'></span>;
+
     return (
         <div className='whatgpt'>
-            <Feature header={header1} text={body1} />
+            <Line />
+            <Feature header={header1} text={body1} styleline='no-line' />
+
             <div className='whatgpt__text-container'>
-                <h2 className='whatgpt__bold-text'>
-                    The possibilitites are beyond your imagination
-                </h2>
-                <p className='whatgpt__text--colored'>Explore the Library</p>
+                <h1 className='whatgpt__bold-text'>
+                    The possibilities are beyond your imagination
+                </h1>
+                <p className='whatgpt__text-colored'>Explore the Library</p>
             </div>
-            <Feature />
-            <Feature />
-            <Feature />
+            <div className='whatgpt__feature-container'>
+                <Feature
+                    header={header2}
+                    text={body2}
+                    styledisplay='display__column'
+                />
+                <Feature
+                    header={header3}
+                    text={body3}
+                    styledisplay='display__column'
+                />
+                <Feature
+                    header={header4}
+                    text={body4}
+                    styledisplay='display__column'
+                />
+            </div>
         </div>
     );
 };
