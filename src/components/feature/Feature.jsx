01bf065c42954cb1ce5c40b7727967d980c0ce4e
Victor Irekponor <marquisvictor93@gmail.com>
2021-11-22 11:52:08 +0100

    increase the top, bottom and side paddings of all the components

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import './feature.css';
 
-const Feature = () => {
-    return <div>Feature</div>;
+const Feature = ({ header, text }) => {
+    return (
+        <>
+            <div className='line'></div>
+            <div className='feature__container'>
+                <h3 className='feature__header'>{header}</h3>
+                <p className='feature__text'>{text}</p>
+            </div>
+        </>
+    );
 };
 
 export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components';
+import './whatGPT3.css';
 
 const WhatGPT3 = () => {
-    return <div>waht</div>;
+    const header1 = 'What is GPT-3';
+    const body1 =
+        'We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.';
+
+    return (
+        <div className='whatgpt'>
+            <Feature header={header1} text={body1} />
+            <div className='whatgpt__text-container'>
+                <h2 className='whatgpt__bold-text'>
+                    The possibilitites are beyond your imagination
+                </h2>
+                <p className='whatgpt__text--colored'>Explore the Library</p>
+            </div>
+            <Feature />
+            <Feature />
+            <Feature />
+        </div>
+    );
 };
 
 export default WhatGPT3;
